ff6b9f54f657a5aee5976ab4281bc83b7b7ca095
Canux CHENG <canuxcheng@gmail.com>
2017-03-03 09:27:18 -0500

    [20170303]update enterprise for rec/res.

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/chengca/myCode/django-wechat-api/.venv"
+set -gx VIRTUAL_ENV "/home/chengca/myCode/django-wechat-api/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- venv/bin/django-admin.py
+++ venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/chengca/myCode/django-wechat-api/.venv/bin/python
+#!/home/chengca/myCode/django-wechat-api/venv/bin/python
 from django.core import management
 
 if __name__ == "__main__":
